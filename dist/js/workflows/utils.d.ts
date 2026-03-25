a659ac4d48b64bdbca4ab1d7c787baf2192c9be9
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2026-03-25 16:24:48 +0200

    chore: update deps

--- dist/js/workflows/utils.d.ts
+++
@@ -1,9 +0,0 @@
-import type { WorkflowBaseUnitSchema } from "@mat3ra/esse/dist/js/types";
-/**
- * @summary set the head of an array of units
- */
-export declare function setUnitsHead<T extends WorkflowBaseUnitSchema>(units: T[]): T[];
-/**
- * @summary Re-establishes the linked `next => flowchartId` logic in an array of units
- */
-export declare function setNextLinks<T extends WorkflowBaseUnitSchema>(units: T[]): T[];
